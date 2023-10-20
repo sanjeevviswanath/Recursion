79dd08cf49cb7ab1eb54d4d80a3882d7e2214670
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-20 14:40:22 -0700

    finished recursion logic for reversal of string

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
+const reverseString = (inputString: string): string => {
+  if (inputString == "") return "";
+
+  return reverseString(inputString.slice(1)) + inputString[0];
+};
+const inputStr = "Sanjeev Viswanath";
+
+console.log("Input string: ", inputStr[0]);
+console.log("hello".slice(1));
+console.log("Reverse of {inputStr} : ", reverseString(inputStr));
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
   <h1>Recursion Tutorial</h1>
+  <h2></h2>
   </div>
 `;
