07d0d4823648253bbfa6cf48a73e8b3203a55632
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-21 21:59:31 -0700

    simple version of palandrom function without recursion

--- src/main.ts
+++ src/main.ts
@@ -14,42 +14,55 @@ import "./style.css";
 
 // REVERSE STRING WITHOUT RECURSION - START
 const reverseString = (inputString: string): string => {
-  return inputString.split('').reverse().join("");
+  return inputString.split("").reverse().join("");
 };
 
 console.log("Reverse of Sanjeev : ", reverseString("Sanjeev Viswanath"));
 // REVERSE STRING WITHOUT RECURSION - END
 // CHECK FOR PALANDROM - START
 
-let inputPalString = "Racecar";
+// let inputPalString = "Racecar";
 
-const checkPalandrom = (inputPalString: string): boolean => {
-  // Logic:
-  //base case: if the length of the inpt string is 1 or if the input string is empty string, then return True.
-  //implement base case
-  if (inputPalString.length === 0 || inputPalString.length === 1) {
-    return true;
-  }
+// const checkPalandrom = (inputPalString: string): boolean => {
+// Logic:
+//base case: if the length of the inpt string is 1 or if the input string is empty string, then return True.
+//implement base case
+//   if (inputPalString.length === 0 || inputPalString.length === 1) {
+//     return true;
+//   }
 
-  // main logic for the smallest problem to solve - where length of string = 2
-  //implement main logic for palandrom
+// main logic for the smallest problem to solve - where length of string = 2
+//implement main logic for palandrom
 
-  if (
-    inputPalString[0].toLowerCase ==
-    inputPalString[inputPalString.length - 1].toLowerCase
-  ) {
-    return checkPalandrom(inputPalString.slice(1, inputPalString.length - 1));
-  }
+//   if (
+//     inputPalString[0].toLowerCase ==
+//     inputPalString[inputPalString.length - 1].toLowerCase
+//   ) {
+//     return checkPalandrom(inputPalString.slice(1, inputPalString.length - 1));
+//   }
 
-  return false;
+//   return false;
+// };
+// console.log(
+//   "The string palandrom check returns:",
+//   inputPalString,
+//   checkPalandrom(inputPalString)
+// );
+// CHECK FOR PALANDROM WITH RECURSION - END
+// CHECK FOR PALANDROM WITHOUT RECURSION - START
+
+const checkPalandrom = (inputString: string): boolean => {
+  return (
+    inputString.toLowerCase() ===
+    inputString.toLowerCase().split("").reverse().join("")
+  );
 };
 console.log(
-  "The string palandrom check returns:",
-  inputPalString,
-  checkPalandrom(inputPalString)
+  "check palandrom for word 'Malayalam'",
+  checkPalandrom("Malayalam")
 );
-// CHECK FOR PALANDROM - END
 
+// CHECK FOR PALANDROM WITHOUT RECURSION - START
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
   <h1>Recursion Tutorial</h1>
